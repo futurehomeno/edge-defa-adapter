73a1c01abf0974b2427299c7d6d3fbefadac215d
Markus Haldorsen <markushaldorsen@gmail.com>
2021-03-04 14:13:17 +0100

    Add all continous logic. Check state every x seconds and send report if something has changed.

--- src/service.go
+++ src/service.go
@@ -82,79 +82,110 @@ func main() {
 
 	pollString := configs.PollTimeSec
 	pollTime, err := strconv.Atoi(pollString)
-	var chargerObject model.ChargerObject
-	loginToken := *&model.LoginToken{}
+	hasInitialized := false
+	// lastStatusObject := model.StatusObject{}
+	// loginToken := *&model.LoginToken{}
 
 	log.Info("Starting ticker...")
 	log.Debug("---------------------------------")
 	ticker := time.NewTicker(time.Duration(pollTime) * time.Second)
 
 	for range ticker.C {
-		if states.Chargers.Data == nil {
-			if configs.AccessToken == "" || configs.UserID == "" {
-				log.Info("User needs to log in.")
-			}
+		counter := 0
+		if states.Chargers.Data == nil && (configs.AccessToken == "" || configs.UserID == "") {
+			log.Info("User needs to log in.")
 		} else {
+			states.Chargers.LastState = nil
+			states.Chargers.LastEnergy = nil
+			states.Chargers.LastTime = nil
+			for _, selectedCharger := range configs.SelectedChargers {
+				for _, chargers := range states.Chargers.Data.ReceivingAccess {
+					for _, chargepoint := range chargers.ChargePoint.AliasMap {
+						if selectedCharger == chargepoint.Name {
+							// if chargepoint.LastStatus != model.SetStatus(chargepoint.Status)
+							if hasInitialized {
+								states.Chargers.LastState = append(states.Chargers.LastState, model.SetStatus(chargepoint.Status))
+								for _, chargeSession := range *states.ChargeSession {
+									if chargeSession.ChargeSession.ChargePointID == chargers.ChargePoint.ID {
+										states.Chargers.LastEnergy = append(states.Chargers.LastEnergy, chargepoint.MeterValue-chargeSession.ChargeSession.MeterStart)
+									} else {
+										states.Chargers.LastEnergy = append(states.Chargers.LastEnergy, 0)
+									}
+									states.Chargers.LastTime = append(states.Chargers.LastTime, chargepoint.StatusUpdated)
+								}
+							} else {
+								states.Chargers.LastState = append(states.Chargers.LastState, "initialized")
+								for range *states.ChargeSession {
+									states.Chargers.LastEnergy = append(states.Chargers.LastEnergy, -1)
+								}
+							}
+						}
+					}
+				}
+			}
+
 			log.Debug("Getting chargers...")
 			var err1 error
-			var err2 error
+			// var err2 error
 			var err3 error
 
 			states.Chargers.Data, err1 = model.GetChargers(configs.UserID, configs.AccessToken)
 			if err1 != nil {
 				log.Error("Error1: ", err1)
 			}
-			states.AliasMap, err2 = model.GetAliasMap(configs.UserID, configs.AccessToken)
-			if err2 != nil {
-				log.Error("Error2: ", err2)
-			}
+			// states.AliasMap, err2 = model.GetAliasMap(configs.UserID, configs.AccessToken)
+			// if err2 != nil {
+			// 	log.Error("Error2: ", err2)
+			// }
 			states.ChargeSession, err3 = model.GetCharging(configs.UserID, configs.AccessToken)
 			if err3 != nil {
 				log.Error("Error3: ", err3)
 			}
-			if err1 != nil || err2 != nil || err3 != nil {
-				log.Error("Something is wrong...: ", err1, err2, err3)
-
+			if err1 != nil || err3 != nil { // || err 2
+				log.Error("Something is wrong...: ", err1, err3) // , err2
 			}
 
 			states.SaveToFile()
 
 			log.Debug("---------------SELECTED CHARGER SECTION BEGINNING---------------")
-			for _, selectedCharger := range configs.SelectedChargers {
-				for p, charging := range *states.ChargeSession {
-					// length := len(states.Chargers.Data.ReceivingAccess)
-					// for p, realCharger := range states.Chargers.Data.ReceivingAccess {
-					// if charging.ChargeSession.ChargePointID == realCharger.ChargePoint.ID {
-					if selectedCharger == charging.ChargeSession.ChargePointID {
-						for _, connector := range states.AliasMap.ReceivingAccess[p].ChargePoint.AliasMap {
-							if len(states.LastChargerObjects) < p {
-								states.LastChargerObjects = append(states.LastChargerObjects, chargerObject)
-								states.LastChargerObjects[p-1].Name = selectedCharger
-							}
-							if len(states.LastChargerObjects) > 0 {
-								// if !reflect.DeepEqual(states.LastChargerObjects[q].Status, connector.Status) {
-								for s, charger := range states.LastChargerObjects {
-									if charger.Name == selectedCharger {
-										if states.LastChargerObjects[s].Status != connector.Status {
-											log.Debug("Old status: ", states.LastChargerObjects[s].Status)
-											log.Debug("New status: ", connector.Status)
-											msgOperatingMode := fimpgo.NewMessage("evt.state.report", "defa", fimpgo.VTypeString, connector.Status, nil, nil, nil)
-											msgOperatingMode.Source = "defa"
-											adrOperatingMode := &fimpgo.Address{
-												MsgType:         fimpgo.MsgTypeEvt,
-												ResourceType:    fimpgo.ResourceTypeDevice,
-												ResourceName:    model.ServiceName,
-												ResourceAddress: "1",
-												ServiceName:     "chargepoint",
-												ServiceAddress:  selectedCharger}
-											mqtt.Publish(adrOperatingMode, msgOperatingMode)
+			//
+			hours := time.Now().UnixNano() / 3600000
 
-											states.LastChargerObjects[s].Status = connector.Status
-										}
-										if states.LastChargerObjects[s].Power != connector.Power {
-											log.Debug("Old power: ", states.LastChargerObjects[s].Power)
-											log.Debug("New power: ", connector.Power)
-											msgPower := fimpgo.NewMessage("evt.power.report", "defa", fimpgo.VTypeString, connector.Power, nil, nil, nil)
+			log.Debug(hours / 1000000)
+
+			//
+
+			for _, selectedCharger := range configs.SelectedChargers {
+				for _, chargers := range states.Chargers.Data.ReceivingAccess {
+					for _, chargepoint := range chargers.ChargePoint.AliasMap {
+						if selectedCharger == chargepoint.Name {
+							if len(states.Chargers.LastEnergy) > 0 {
+								log.Debug("last state 0: ", states.Chargers.LastState[counter])
+								log.Debug("defa state 0: ", chargepoint.Status)
+								log.Debug("new state 0: ", model.SetStatus(chargepoint.Status))
+								if states.Chargers.LastState[counter] != model.SetStatus(chargepoint.Status) {
+									log.Debug("last state 1: ", states.Chargers.LastState[counter])
+									log.Debug("new state 1: ", model.SetStatus(chargepoint.Status))
+									msgOperatingMode := fimpgo.NewMessage("evt.state.report", "defa", fimpgo.VTypeString, model.SetStatus((chargepoint.Status)), nil, nil, nil)
+									msgOperatingMode.Source = "defa"
+									adrOperatingMode := &fimpgo.Address{
+										MsgType:         fimpgo.MsgTypeEvt,
+										ResourceType:    fimpgo.ResourceTypeDevice,
+										ResourceName:    model.ServiceName,
+										ResourceAddress: "1",
+										ServiceName:     "chargepoint",
+										ServiceAddress:  selectedCharger}
+									mqtt.Publish(adrOperatingMode, msgOperatingMode)
+								}
+							}
+							for _, chargeSession := range *states.ChargeSession {
+								if chargeSession.ChargeSession.ChargePointID == chargers.ChargePoint.ID {
+									chargeEnergy := chargepoint.MeterValue - chargeSession.ChargeSession.MeterStart
+									if len(states.Chargers.LastEnergy) > 0 {
+										if states.Chargers.LastEnergy[counter] != chargeEnergy {
+											log.Debug("last energy 1: ", states.Chargers.LastEnergy[counter])
+											log.Debug("new energy 1: ", chargeEnergy)
+											msgPower := fimpgo.NewMessage("evt.current_session.report", "defa", fimpgo.VTypeFloat, chargeEnergy, nil, nil, nil)
 											msgPower.Source = "defa"
 											adrPower := &fimpgo.Address{
 												MsgType:         fimpgo.MsgTypeEvt,
@@ -165,22 +196,20 @@ func main() {
 												ServiceAddress:  selectedCharger}
 											mqtt.Publish(adrPower, msgPower)
 
-											states.LastChargerObjects[s].Power = connector.Power
+											newMeterValue := (states.Chargers.LastEnergy[counter] - chargeEnergy) / float64((chargepoint.StatusUpdated-states.Chargers.LastTime[counter])/1000000)
+											log.Debug("New meter value: ", newMeterValue)
 										}
 									}
 								}
-							} else {
-								states.LastChargerObjects = append(states.LastChargerObjects, chargerObject)
 							}
+							counter++
 						}
-						// if i >= length {
-						// 	continue
 					}
-					states.SaveToFile()
 				}
 			}
 			log.Debug("---------------SELECTED CHARGER SECTION ENDED---------------")
 		}
+		hasInitialized = true
 	}
 
 	for {
