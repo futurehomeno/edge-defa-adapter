d381c6d7e11f0edd61ce5cbb0f40567df8201d4e
Markus Haldorsen <markushaldorsen@gmail.com>
2021-03-04 14:12:34 +0100

    API structure update. Handle dynamic keys fix. Fix stop and start charging functions.

--- src/model/api.go
+++ src/model/api.go
@@ -23,116 +23,120 @@ const (
 
 type (
 	aliasMapValue struct {
-		Connector         int
-		Power             float64
-		MeterValue        float64 `json:"meterValue"`
-		ConnectorType     string  `json:"connectorType"`
-		Status            string
-		ErrorCode         string `json:"errorCode"`
-		StatusUpdate      int64  `json:"statusUpdated"`
-		IsFavorite        bool   `json:"isFavorite"`
-		LastChargerObject ChargerObject
+		Name          string
+		Connector     int     `json:"connector"`
+		Power         float64 `json:"power"`
+		MeterValue    float64 `json:"meterValue"`
+		ConnectorType string  `json:"connectorType"`
+		Status        string  `json:"status"`
+		ErrorCode     string  `json:"errorCode"`
+		StatusUpdated int64   `json:"statusUpdated"`
+		IsFavorite    bool    `json:"isFavorite"`
+		LastStatus    string  `json:"lastStatus"`
 	}
 	Charger2 struct {
 		TimeStamp       int64 `json:"timestamp"`
 		ReceivingAccess []struct {
 			ChargePoint struct {
-				AliasMap map[string]aliasMapValue `json:"aliasMap"`
+				AliasMap map[string]*aliasMapValue `json:"aliasMap"`
 			} `json:"chargePoint"`
 		} `json:"receivingAccess"`
 	}
-)
 
-type Chargers struct {
-	Data *Charger
-}
-
-type ResetToken struct {
-	Token    string `json:"token"`
-	DevToken string `json:"devToken,omitempty"`
-}
-
-type LoginToken struct {
-	Token    string `json:"token"`
-	DevToken string `json:"devToken,omitempty"`
-}
-
-type Charger struct {
-	Timestamp       int64 `json:"timestamp"`
-	ReceivingAccess []struct {
-		ChargePoint struct {
-			ID                string `json:"id"`
-			Group             string `json:"group"`
-			LastHB            int64  `json:"lastHB"`
-			HbTimeout         bool   `json:"hbTimeout"`
-			ConfigurationKeys struct {
-				ChargingScheduleAllowedChargingRateUnit string `json:"ChargingScheduleAllowedChargingRateUnit"`
-				SupportedFeatureProfiles                string `json:"SupportedFeatureProfiles"`
-			} `json:"configurationKeys"`
-			LastSuccesfulChargingRate float64  `json:"lastSuccesfulChargingRate"`
-			AliasMap                  struct{} `json:"aliasMap"` // This will be empty, as it contains dynamic keys.
-			IsFavorite                bool     `json:"isFavorite"`
-			IsReservedForYou          bool     `json:"isReservedForYou"`
-			LoadBalancingActive       bool     `json:"loadBalancingActive"`
-		} `json:"chargePoint"`
-		Token struct {
-			Status        string      `json:"status"`
-			AccessID      string      `json:"accessId"`
-			ChargePointID string      `json:"chargePointId"`
-			ConnectorID   interface{} `json:"connectorId"`
-			EndTime       interface{} `json:"endTime"`
-			StartTime     int64       `json:"startTime"`
-			Role          string      `json:"role"`
-			MetaString    interface{} `json:"metaString"`
-		} `json:"token"`
-	} `json:"receivingAccess"`
-	GivingAccess []interface{} `json:"givingAccess"`
-}
-
-type Charging []struct {
-	ChargeSession struct {
-		StartTime   int64       `json:"startTime"`
-		EndTime     interface{} `json:"endTime"`
-		MeterStart  float64     `json:"meterStart"`
-		MeterEnd    float64     `json:"meterEnd"`
-		PaymentInfo struct {
-			Cost     float64     `json:"cost"`
-			Vat      float64     `json:"vat"`
-			Currency interface{} `json:"currency"`
-		} `json:"paymentInfo"`
-		TransactionID  int         `json:"transactionId"`
-		UserID         string      `json:"userId"`
-		ChargePointID  string      `json:"chargePointId"`
-		ConnectorID    int         `json:"connectorId"`
-		Classification string      `json:"classification"`
-		Note           interface{} `json:"note"`
-		CarInfo        interface{} `json:"carInfo"`
-		CreditCardInfo interface{} `json:"creditCardInfo"`
-		IsMyCharger    bool        `json:"isMyCharger"`
-		Metadata       struct {
-			Cpid                string `json:"cpid"`
-			LocationDescription string `json:"locationDescription"`
-			Location            string `json:"location"`
-		} `json:"metadata"`
-	} `json:"chargeSession"`
-}
-
-type AliasMap []struct {
-	NumXXX struct {
-		Connector     int         `json:"connector"`
-		Power         float64     `json:"power"`
-		MeterValue    float64     `json:"meterValue"`
-		Info          interface{} `json:"info"`
-		ConnectorType string      `json:"connectorType"`
-		Tariff        interface{} `json:"tariff"`
-		Status        string      `json:"status"`
-		ErrorCode     string      `json:"errorCode"`
-		ErrorInfo     interface{} `json:"errorInfo"`
-		StatusUpdated int64       `json:"statusUpdated"`
-		CustomerID    interface{} `json:"customerId"`
-		IsFavorite    bool        `json:"isFavorite"`
+	Chargers struct {
+		LastState  []string
+		LastEnergy []float64
+		LastTime   []int64
+		Data       *Charger
 	}
-}
+
+	ResetToken struct {
+		Token    string `json:"token"`
+		DevToken string `json:"devToken,omitempty"`
+	}
+
+	LoginToken struct {
+		Token    string `json:"token"`
+		DevToken string `json:"devToken,omitempty"`
+	}
+
+	Charger struct {
+		Timestamp       int64 `json:"timestamp"`
+		ReceivingAccess []struct {
+			ChargePoint struct {
+				ID                string `json:"id"`
+				Group             string `json:"group"`
+				LastHB            int64  `json:"lastHB"`
+				HbTimeout         bool   `json:"hbTimeout"`
+				ConfigurationKeys struct {
+					ChargingScheduleAllowedChargingRateUnit string `json:"ChargingScheduleAllowedChargingRateUnit"`
+					SupportedFeatureProfiles                string `json:"SupportedFeatureProfiles"`
+				} `json:"configurationKeys"`
+				LastSuccesfulChargingRate float64                   `json:"lastSuccesfulChargingRate"`
+				AliasMap                  map[string]*aliasMapValue `json:"aliasMap"`
+				IsFavorite                bool                      `json:"isFavorite"`
+				IsReservedForYou          bool                      `json:"isReservedForYou"`
+				LoadBalancingActive       bool                      `json:"loadBalancingActive"`
+			} `json:"chargePoint"`
+			Token struct {
+				Status        string      `json:"status"`
+				AccessID      string      `json:"accessId"`
+				ChargePointID string      `json:"chargePointId"`
+				ConnectorID   interface{} `json:"connectorId"`
+				EndTime       interface{} `json:"endTime"`
+				StartTime     int64       `json:"startTime"`
+				Role          string      `json:"role"`
+				MetaString    interface{} `json:"metaString"`
+			} `json:"token"`
+		} `json:"receivingAccess"`
+		GivingAccess []interface{} `json:"givingAccess"`
+	}
+
+	Charging []struct {
+		ChargeSession struct {
+			StartTime   int64       `json:"startTime"`
+			EndTime     interface{} `json:"endTime"`
+			MeterStart  float64     `json:"meterStart"`
+			MeterEnd    float64     `json:"meterEnd"`
+			PaymentInfo struct {
+				Cost     float64     `json:"cost"`
+				Vat      float64     `json:"vat"`
+				Currency interface{} `json:"currency"`
+			} `json:"paymentInfo"`
+			TransactionID  int         `json:"transactionId"`
+			UserID         string      `json:"userId"`
+			ChargePointID  string      `json:"chargePointId"`
+			ConnectorID    int         `json:"connectorId"`
+			Classification string      `json:"classification"`
+			Note           interface{} `json:"note"`
+			CarInfo        interface{} `json:"carInfo"`
+			CreditCardInfo interface{} `json:"creditCardInfo"`
+			IsMyCharger    bool        `json:"isMyCharger"`
+			Metadata       struct {
+				Cpid                string `json:"cpid"`
+				LocationDescription string `json:"locationDescription"`
+				Location            string `json:"location"`
+			} `json:"metadata"`
+		} `json:"chargeSession"`
+	}
+
+	AliasMap []struct {
+		NumXXX struct {
+			Connector     int         `json:"connector"`
+			Power         float64     `json:"power"`
+			MeterValue    float64     `json:"meterValue"`
+			Info          interface{} `json:"info"`
+			ConnectorType string      `json:"connectorType"`
+			Tariff        interface{} `json:"tariff"`
+			Status        string      `json:"status"`
+			ErrorCode     string      `json:"errorCode"`
+			ErrorInfo     interface{} `json:"errorInfo"`
+			StatusUpdated int64       `json:"statusUpdated"`
+			CustomerID    interface{} `json:"customerId"`
+			IsFavorite    bool        `json:"isFavorite"`
+		}
+	}
+)
 
 func processHTTPResponse(resp *http.Response, err error, holder interface{}) error {
 	if err != nil {
@@ -155,21 +159,12 @@ func processHTTPResponse(resp *http.Response, err error, holder interface{}) err
 func StopCharging(deviceId string, connector int, userId string, accessToken string) error {
 	log.Debug("Stopping charging session")
 
-	type Payload struct {
-		ID        string `json:"cpid"`
-		Connector int    `json:"connector"`
-	}
-	data := Payload{
-		ID:        deviceId,
-		Connector: connector,
-	}
-	payloadBytes, err := json.Marshal(data)
-	if err != nil {
-		log.Debug("issue with payloadbytes")
-	}
-	body := bytes.NewReader(payloadBytes)
-
-	req, err := http.NewRequest("POST", stop, body)
+	payload := strings.NewReader(fmt.Sprintf(`{
+		"cpid": "%s",
+		"connector": %d
+	}`, deviceId, connector))
+	log.Debug("Stop body: ", payload)
+	req, err := http.NewRequest("POST", start, payload)
 
 	if err != nil {
 		log.Error(fmt.Errorf("Can't stop charging, error: "), err)
@@ -181,12 +176,11 @@ func StopCharging(deviceId string, connector int, userId string, accessToken str
 	req.Header.Set("X-Authorization", accessToken)
 	req.Header.Set("devToken", devToken)
 	resp, err := http.DefaultClient.Do(req)
-
+	if err != nil {
+		log.Error(err)
+	}
 	defer resp.Body.Close()
-	log.Debug("Request", req)
-	log.Debug("Req body", req.Body)
-	log.Debug("Response", resp)
-	log.Debug("Resp body", resp.Body)
+
 	if resp.StatusCode != 200 {
 		//bytes, _ := ioutil.ReadAll(resp.Body)
 		log.Error("Bad HTTP return code ", resp.StatusCode)
@@ -268,37 +262,14 @@ func GetChargers(userId string, accessToken string) (*Charger, error) {
 	if err != nil {
 		return charger, err
 	}
-	// err = get(userId, accessToken, mychargers, charger, true) // get aliasMap
+	for _, charger := range charger.ReceivingAccess {
+		for key := range charger.ChargePoint.AliasMap {
+			log.Debug("key: ", key)
+			charger.ChargePoint.AliasMap[key].Name = key
+		}
+	}
 
 	return charger, err
-}
-
-func GetAliasMap(userId string, accessToken string) (*Charger2, error) {
-	// aliasMap := new(AliasMap)
-	var c *Charger2
-	// var am AliasMap
-	log.Debug("Getting from ", mychargers)
-
-	req, err := http.NewRequest("GET", mychargers, nil)
-
-	if err != nil {
-		log.Error(fmt.Errorf("Can't GET from ", mychargers))
-	}
-
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("X-User", userId)
-	req.Header.Set("X-Authorization", accessToken)
-	req.Header.Set("devToken", devToken)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&c); err != nil {
-		log.Error(err)
-	}
-
-	return c, err
 }
 
 func GetCharging(userId string, accessToken string) (*Charging, error) {
@@ -328,10 +299,15 @@ func get(userId string, accessToken string, url string, target interface{}) erro
 	return err
 }
 
-func StartCharging(deviceId string, userId string, accessToken string) error {
+func StartCharging(deviceId string, connector int, userId string, accessToken string) error {
 	log.Debug("Starting charging session")
 
-	req, err := http.NewRequest("POST", start, nil)
+	payload := strings.NewReader(fmt.Sprintf(`{
+		"cpid": "%s",
+		"connector": %d
+	}`, deviceId, connector))
+	log.Debug("Start body: ", payload)
+	req, err := http.NewRequest("POST", start, payload)
 
 	if err != nil {
 		log.Error(fmt.Errorf("Can't start charging, error: "), err)
@@ -343,7 +319,9 @@ func StartCharging(deviceId string, userId string, accessToken string) error {
 	req.Header.Set("X-Authorization", accessToken)
 	req.Header.Set("devToken", devToken)
 	resp, err := http.DefaultClient.Do(req)
-
+	if err != nil {
+		log.Error(err)
+	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
